82681d02ae70e3662bef1479f96e8d297ec29e32
Jelon1211 <jelongames@gmail.com>
2023-09-30 15:50:27 +0200

    login init

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="dark:bg-slate-800">
-        <main className="px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+      <body className="bg-slate-900">
+        <main className="px-4 md:px-6 prose prose-xl prose-slate prose-invert mx-auto">
           {children}
         </main>
       </body>
